392c4ebbf5553c3a898b47bc0f791c85518f7bcd
Karthikeyan <ccl.karthikeyan@gmail.com>
2022-09-22 19:59:25 +0530

    crud created by usng api

--- javascript .crud/index.js
+++ javascript .crud/index.js
@@ -1,116 +1,114 @@
 function loadTable() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "	https://dummy.restapiexample.com/api/v1/employees");
-    xhttp.send();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        // console.log(this.responseText);
-        var trHTML = ''; 
-        const objects = JSON.parse(this.responseText);
-        const employee = objects.data
-        for (let object of employee) {
-          trHTML += '<tr>'; 
-          trHTML += '<td>'+object['id']+'</td>';
-          trHTML += '<td>'+object['employee_name']+'</td>';
-          trHTML += '<td>'+object['employee_salary']+'</td>';
-          trHTML += '<td>'+object['employee_age']+'</td>';
-          trHTML += '<td>'+object['profile_image']+'</td>';
-          trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox('+object['id']+')">Edit</button>';
-          trHTML += '<button type="button" class="btn btn-outline-danger" onclick="userDelete('+object['id']+')">Del</button></td>';
-          trHTML += "</tr>";
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://www.mecallapi.com/api/users");
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var trHTML = ''; 
+      const objects = JSON.parse(this.responseText);
+      for (let object of objects) {
+        trHTML += '<tr>'; 
+        trHTML += '<td>'+object['id']+'</td>';
+        trHTML += '<td><img width="50px" src="'+object['avatar']+'" class="avatar"></td>';
+        trHTML += '<td>'+object['fname']+'</td>';
+        trHTML += '<td>'+object['lname']+'</td>';
+        trHTML += '<td>'+object['username']+'</td>';
+        trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox('+object['id']+')">Edit</button>';
+        trHTML += '<button type="button" class="btn btn-outline-danger" onclick="userDelete('+object['id']+')">Del</button></td>';
+        trHTML += "</tr>";
+      }
+      document.getElementById("mytable").innerHTML = trHTML;
+    }
+  };
+}
+
+loadTable();
+function showUserCreateBox() {
+  Swal.fire({
+    title: 'Create user',
+    html:
+      '<input id="id" type="hidden">' +
+      '<input id="fname" class="swal2-input" placeholder="First">' +
+      '<input id="lname" class="swal2-input" placeholder="Last">' +
+      '<input id="username" class="swal2-input" placeholder="Username">' +
+      '<input id="email" class="swal2-input" placeholder="Email">',
+    focusConfirm: false,
+    preConfirm: () => {
+      userCreate();
+    }
+  })
+}
+
+function userCreate() {
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+    
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://www.mecallapi.com/api/users/create");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({ 
+    "fname": fname, "lname": lname, "username": username, "email": email, 
+    "avatar": "https://www.mecallapi.com/users/cat.png"
+  }));
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      Swal.fire(objects['message']);
+      loadTable();
+    }
+  };
+}
+function showUserEditBox(id) {
+  console.log(id);
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://www.mecallapi.com/api/users/"+id);
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      const user = objects['user'];
+      console.log(user);
+      Swal.fire({
+        title: 'Edit User',
+        html:
+          '<input id="id" type="hidden" value='+user['id']+'>' +
+          '<input id="fname" class="swal2-input" placeholder="First" value="'+user['fname']+'">' +
+          '<input id="lname" class="swal2-input" placeholder="Last" value="'+user['lname']+'">' +
+          '<input id="username" class="swal2-input" placeholder="Username" value="'+user['username']+'">' +
+          '<input id="email" class="swal2-input" placeholder="Email" value="'+user['email']+'">',
+        focusConfirm: false,
+        preConfirm: () => {
+          userEdit();
         }
-        document.getElementById("mytable").innerHTML = trHTML;
-      }
-    };
-  }
-  
-  loadTable();
-  function showUserCreateBox() {
-    Swal.fire({
-      title: 'Create user',
-      html:
-        '<input id="id" type="hidden">' +
-        '<input id="employee_name" class="swal2-input" placeholder="Name">' +
-        '<input id="employee_salary" class="swal2-input" placeholder="Salary">' +
-        '<input id="employee_age" class="swal2-input" placeholder="Age">' +
-        '<input id="profile_image" class="swal2-input" placeholder="Image">',
-      focusConfirm: false,
-      preConfirm: () => {
-        userCreate();
-      }
-    })
-  }
-  
-  function userCreate() {
-    const fname = document.getElementById("employee_name").value;
-    const lname = document.getElementById("employee_salary").value;
-    const username = document.getElementById("employee_age").value;
-    const email = document.getElementById("profile_image").value;
-      
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "	https://dummy.restapiexample.com/api/v1/create");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify({ 
-      "employee_name": fname, "employee_salary": lname, "employee_age": username, "profile_image": email
-   
-    }));
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        const objects = JSON.parse(this.responseText);
-        Swal.fire(objects['message']);
-        loadTable();
-      }
-    };
-  }
-  function showUserEditBox(id) {
-    console.log(id);
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "	https://dummy.restapiexample.com/api/v1/employee/"+id);
-    xhttp.send();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        const objects = JSON.parse(this.responseText);
-        const user = objects['user'];
-        console.log(user);
-        Swal.fire({
-          title: 'Edit User',
-          html:
-            '<input id="id" type="hidden" value='+user['id']+'>' +
-            '<input id="fname" class="swal2-input" placeholder="First" value="'+user['fname']+'">' +
-            '<input id="lname" class="swal2-input" placeholder="Last" value="'+user['lname']+'">' +
-            '<input id="username" class="swal2-input" placeholder="Username" value="'+user['username']+'">' +
-            '<input id="email" class="swal2-input" placeholder="Email" value="'+user['email']+'">',
-          focusConfirm: false,
-          preConfirm: () => {
-            userEdit();
-          }
-        })
-      }
-    };
-  }
-  
-  function userEdit() {
-    const id = document.getElementById("id").value;
-    const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-      
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", "https://www.mecallapi.com/api/users/update");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify({ 
-      "id": id, "fname": fname, "lname": lname, "username": username, "email": email, 
-      "avatar": "https://www.mecallapi.com/users/cat.png"
-    }));
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        const objects = JSON.parse(this.responseText);
-        Swal.fire(objects['message']);
-        loadTable();
-      }
-    };
-  }
+      })
+    }
+  };
+}
+
+function userEdit() {
+  const id = document.getElementById("id").value;
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+    
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("PUT", "https://www.mecallapi.com/api/users/update");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({ 
+    "id": id, "fname": fname, "lname": lname, "username": username, "email": email, 
+    "avatar": "https://www.mecallapi.com/users/cat.png"
+  }));
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      Swal.fire(objects['message']);
+      loadTable();
+    }
+  };
+}
   function userDelete(id) {
     const xhttp = new XMLHttpRequest();
     xhttp.open("DELETE", "https://www.mecallapi.com/api/users/delete");
--- javascript api/demo.js
+++ javascript api/demo.js
@@ -5,31 +5,26 @@ function GetTable() {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status === 200) {
       data = JSON.parse(this.responseText);
-      // console.log(this.responseText);
       trHTML = '';
       const Details = data;
       console.log(data, 'data');
       for (let detail of Details) {
         trHTML += `<tr>`;
-        trHTML += `<td> ${detail[`_id`]} </td>`;
-        trHTML += `<td> ${detail[`first_name`]} </td>`;
-        trHTML += `<td> ${detail[`last_name`]} </td>`;
-        trHTML += `<td> ${detail[`email`]} </td>`;
-        trHTML += `<td> ${detail[`mobile`]} </td>`;
-        trHTML += `<td><button class="btn1" type="button" (${
-          detail[`_id`]
-        })>Edit</button>&nbsp;&nbsp;`;
-        trHTML += `<button class="btn2" type="button" ( ${
-          detail[`_id`]
-        })>Del</button></td>`;
+        trHTML += `<td> ${detail._id} </td>`;
+        trHTML += `<td> ${detail.first_name} </td>`;
+        trHTML += `<td> ${detail.last_name} </td>`;
+        trHTML += `<td> ${detail.email} </td>`;
+        trHTML += `<td> ${detail.mobile} </td>`;
+        trHTML += `<td><button class="btn1" type="button" onclick="userEdit('${detail['_id']}')">Edit</button>&nbsp;&nbsp;`;
+        trHTML += `<button class="btn2" type="button" onclick="userDelete('${detail['_id']}')">Del</button></td>`;
         trHTML += `</tr>`;
       }
-     const type = document.getElementById('data');
-     if (type != null) {
-      details = type.innerHTML= trHTML;
-    } else {
-      details = null;
-    }
+      const type = document.getElementById('data');
+      if (type != null) {
+        details = type.innerHTML = trHTML;
+      } else {
+        details = null;
+      }
     }
   };
 }
@@ -43,15 +38,15 @@ function userCreate() {
 
       const element = document.getElementById('fname');
       if (element != null) {
-        fname = element.value;
+        first_name = element.value;
       } else {
-        fname = null;
+        first_name = null;
       }
       const element1 = document.getElementById('lname');
       if (element1 != null) {
-        lname = element1.value;
+        last_name = element1.value;
       } else {
-        lname = null;
+        last_name = null;
       }
       const element2 = document.getElementById('email');
       if (element2 != null) {
@@ -66,23 +61,89 @@ function userCreate() {
         mobile = null;
       }
       const xhttp = new XMLHttpRequest();
-      xhttp.open('POST', 'https://dummy.restapiexample.com/api/v1/create');
+      xhttp.open('POST', 'http://localhost:3000/user/add');
       xhttp.setRequestHeader('Content-Type', 'application/json');
       xhttp.send(
         JSON.stringify({
-          fname: fname,
-          lname: lname,
+          first_name: first_name,
+          last_name: last_name,
           email: email,
           mobile: mobile,
         })
       );
       xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status === 200) {
-          const objects = JSON.parse(this.responseText);
-          console.log(this.responseText, 'post');
+          objects = JSON.parse(this.responseText);
+          alert('Successfully Created');
+
+          // console.log(this.responseText, 'post');
         }
       };
     });
   }
 }
 userCreate();
+
+function userDelete(_id) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('DELETE', 'http://localhost:3000/user/' + _id);
+  xhttp.send(
+    JSON.stringify({
+      _id: _id,
+    })
+  );
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      data = JSON.parse(xhttp.responseText);
+      alert('Deleted successfully');
+      // location.reload();
+    }
+  };
+}
+
+function userEdit(_id) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('GET', 'http://localhost:3000/user/' + _id);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      const data = JSON.parse(xhttp.responseText);
+      console.log(data);
+      setFormData( data._id,data.first_name, data.last_name, data.email, data.mobile);
+      console.log(data, 'update');
+    }
+  };
+}
+function setFormData(_id,first_name, last_name, email, mobile) {
+  document.getElementById('id').value = _id;
+  document.getElementById('fname').value = first_name;
+  document.getElementById('lname').value = last_name;
+  document.getElementById('email').value = email;
+  document.getElementById('mobile').value = mobile;
+  // clearFormData();
+}
+function userUpdate(_id) {
+  const id = document.getElementById('id').value;
+  const fname = document.getElementById('fname').value;
+  const lname = document.getElementById('lname').value;
+  const email = document.getElementById('email').value;
+  const mobile = document.getElementById('mobile').value;
+
+  console.log(id,fname, lname, email, mobile, 'update');
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('PUT', 'http://localhost:3000/user/update/' + id);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send(
+    JSON.stringify({
+      first_name: fname,              
+      last_name: lname,
+      email: email,
+      mobile: mobile,
+    })
+  );
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      data = JSON.parse(xhttp.responseText);
+    }
+  };
+}
