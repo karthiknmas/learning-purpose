cf807f0f25976ff62ba5609f8fbd12f7a862b15a
Karthikeyan <ccl.karthikeyan@gmail.com>
2022-09-26 21:05:31 +0530

    validated crud

--- javascript api/demo.js
+++ javascript api/demo.js
@@ -10,12 +10,12 @@ function GetTable() {
       console.log(data, 'data');
       for (let detail of Details) {
         trHTML += `<tr>`;
-        trHTML += `<td> ${detail._id} </td>`;
+        trHTML += `<td></td>`;
         trHTML += `<td> ${detail.first_name} </td>`;
         trHTML += `<td> ${detail.last_name} </td>`;
         trHTML += `<td> ${detail.email} </td>`;
         trHTML += `<td> ${detail.mobile} </td>`;
-        trHTML += `<td><button class="btn1" type="button" onclick="userEdit('${detail['_id']}')">Edit</button>&nbsp;&nbsp;`;
+        trHTML += `<td><a onclick="formSubmit();showButton();"><button class="btn1" id="class" type="button" onclick="userEdit('${detail['_id']}')">Edit</button></a>&nbsp;&nbsp;`;
         trHTML += `<button class="btn2" type="button" onclick="userDelete('${detail['_id']}')">Del</button></td>`;
         trHTML += `</tr>`;
       }
@@ -42,11 +42,25 @@ function userCreate() {
       } else {
         first_name = null;
       }
+      if (element.value == '') {
+        document.getElementById('fnameError').classList.remove('hide');
+      } else {
+        if (!document.getElementById('fnameError').classList.contains('hide')) {
+          document.getElementById('fnameError').classList.add('hide');
+        }
+      }
       const element1 = document.getElementById('lname');
       if (element1 != null) {
         last_name = element1.value;
       } else {
         last_name = null;
+      }
+      if (element1.value == '') {
+        document.getElementById('lnameError').classList.remove('hide');
+      } else {
+        if (!document.getElementById('lnameError').classList.contains('hide')) {
+          document.getElementById('lnameError').classList.add('hide');
+        }
       }
       const element2 = document.getElementById('email');
       if (element2 != null) {
@@ -54,11 +68,27 @@ function userCreate() {
       } else {
         email = null;
       }
+      if (element2.value == '') {
+        document.getElementById('emailError').classList.remove('hide');
+      } else {
+        if (!document.getElementById('emailError').classList.contains('hide')) {
+          document.getElementById('emailError').classList.add('hide');
+        }
+      }
       const element3 = document.getElementById('mobile');
       if (element3 != null) {
         mobile = element3.value;
       } else {
         mobile = null;
+      }
+      if (element3.value == '') {
+        document.getElementById('mobileError').classList.remove('hide');
+      } else {
+        if (
+          !document.getElementById('mobileError').classList.contains('hide')
+        ) {
+          document.getElementById('mobileError').classList.add('hide');
+        }
       }
       const xhttp = new XMLHttpRequest();
       xhttp.open('POST', 'http://localhost:3000/user/add');
@@ -71,12 +101,23 @@ function userCreate() {
           mobile: mobile,
         })
       );
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status === 200) {
-          objects = JSON.parse(this.responseText);
-          alert('Successfully Created');
-
-          // console.log(this.responseText, 'post');
+      xhttp.onreadystatechange = function (res) {
+        if (
+          (res.target.responseText =
+            '[{"message":"\\"email\\" must be a valid email","path":["email"],"type":"string.email","context":{"value":"hjk","invalids":["hjk"],"label":"email","key":"email"}}]')
+        ) {
+          document.getElementById('mob').innerHTML =
+            'Check Your Email/ Mobile Number';
+        }
+        if (res.target.status === 201) {
+          document.getElementById('message').innerHTML =
+            'Created successfully.';
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        } else if (res.target.status === 404) {
+          document.getElementById('message1').innerHTML =
+            'Note: Given Data Already Exist. Please retry';
         }
       };
     });
@@ -93,10 +134,12 @@ function userDelete(_id) {
     })
   );
   xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
+    if (xhttp.readyState == 4 && xhttp.status === 200) {
       data = JSON.parse(xhttp.responseText);
-      alert('Deleted successfully');
-      // location.reload();
+      document.getElementById('message').innerHTML = 'Deleted successfully';
+      setTimeout(function () {
+        window.location.reload();
+      }, 2000);
     }
   };
 }
@@ -108,20 +151,25 @@ function userEdit(_id) {
   xhttp.onreadystatechange = function () {
     if (xhttp.readyState === 4 && xhttp.status === 200) {
       const data = JSON.parse(xhttp.responseText);
-      console.log(data);
-      setFormData( data._id,data.first_name, data.last_name, data.email, data.mobile);
-      console.log(data, 'update');
+      setFormData(
+        data._id,
+        data.first_name,
+        data.last_name,
+        data.email,
+        data.mobile
+      );
     }
   };
 }
-function setFormData(_id,first_name, last_name, email, mobile) {
+
+function setFormData(_id, first_name, last_name, email, mobile) {
   document.getElementById('id').value = _id;
   document.getElementById('fname').value = first_name;
   document.getElementById('lname').value = last_name;
   document.getElementById('email').value = email;
   document.getElementById('mobile').value = mobile;
-  // clearFormData();
 }
+
 function userUpdate(_id) {
   const id = document.getElementById('id').value;
   const fname = document.getElementById('fname').value;
@@ -129,21 +177,36 @@ function userUpdate(_id) {
   const email = document.getElementById('email').value;
   const mobile = document.getElementById('mobile').value;
 
-  console.log(id,fname, lname, email, mobile, 'update');
   const xhttp = new XMLHttpRequest();
   xhttp.open('PUT', 'http://localhost:3000/user/update/' + id);
   xhttp.setRequestHeader('Content-Type', 'application/json');
   xhttp.send(
     JSON.stringify({
-      first_name: fname,              
+      first_name: fname,
       last_name: lname,
       email: email,
       mobile: mobile,
     })
   );
   xhttp.onreadystatechange = function () {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
+    if (xhttp.readyState == 4 && xhttp.status === 200) {
       data = JSON.parse(xhttp.responseText);
+      document.getElementById('message').innerHTML = 'Updated successfully.';
+      setTimeout(function () {
+        window.location.reload();
+      }, 2000);
     }
   };
+}
+function formSubmit() {
+  document.getElementById('div3').style.display = 'inline-block';
+}
+function showButton() {
+  document.getElementById('div2').style.display = 'none';
+}
+function hideButton() {
+  if (!userUpdate()) {
+    document.getElementById('message1').style.display = 'none';
+    document.getElementById('mob').style.display = 'none';
+  }
 }
--- loop/demo.js
+++ loop/demo.js
@@ -1,16 +1,75 @@
-Name = [
-  'rakesh',
-  'hareesh',
-  'suresh',
-  'kamesh',
-  'vignesh',
-  'sathish',
-  'ganesh',
-  'jaivish',
-  'makesh',
-];
-let i = 0;
-for (i = 0; i < Name.length; i++) {
-
-    console.log(Name[i]);
+const tempDatas = ['Ajay', 'Data', 'Type', 'Const', 'Get'];
+let j = tempDatas.length;
+let text = '';
+for (i = 0; i < j; i++) {
+  text += tempDatas[i];
+  // console.log(tempDatas[i]);
 }
+const dataSet = tempDatas.reduce((acc, data) => acc + data);
+console.log(dataSet, 'reduce');
+console.log(text);
+document.getElementById('demo').innerHTML = text;
+
+// Immediately Invoked Function Expression
+const a = 10;
+
+(function () {
+  const a = 100;
+  console.log(a);
+})();
+console.log(a);
+function c() {
+  let count = 0;
+  document.getElementById('demo1').addEventListener('click', () => {
+    console.log('Button clicked', ++count);
+  });
+}
+c();
+function x() {
+  console.log('a');
+}
+x();
+
+y = () => {
+  console.log('b');
+};
+y();
+(function () {
+  console.log('c');
+})();
+
+setTimeout(function () {
+  console.log('Timer');
+}, 1000);
+
+let start = new Date().getTime();
+let end = start;
+
+while (end < start + 5000) {
+  end = new Date().getTime();
+}
+console.log('start');
+// callback function
+
+u = (name) => {
+  return `Good morning ${name}`;
+};
+v = (name) => {
+  return `Good night ${name}`;
+};
+l = (name, callback) => {
+  const myName = 'Hareesh';
+  console.log(`${callback(name)},This is: ${myName}`);
+};
+l('Kavin', u);
+l('Kavi', v);
+ 
+let global = 'hello world';
+
+setGlobal=()=>{
+  global = 'i am there';
+}
+// setGlobal(); 
+console.log(global);
+
+
